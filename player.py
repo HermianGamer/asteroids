8fa02b823d53c4b2e8f4c79ca3b03759bf6e3eb1
HermianGamer <gianfranco.scarpatiperez@gmail.com>
2026-04-24 22:33:56 -0500

    add asteroid split method

--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shotcooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -40,8 +41,14 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
 
+        self.shotcooldown -= dt
+
     def shoot(self):
+        if self.shotcooldown > 0:
+            print("Cooling down gun...")
+            return
         shot = Shot(self.position.x, self.position.y)
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation) * PLAYER_SHOOT_SPEED
         shot.velocity = rotated_vector
+        self.shotcooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
