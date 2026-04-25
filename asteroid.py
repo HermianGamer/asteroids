b7054be68447cb7489b8645c64b832ff883216ad
HermianGamer <gianfranco.scarpatiperez@gmail.com>
2026-04-25 09:06:32 -0500

    update asteroids color

--- asteroid.py
+++ asteroid.py
@@ -2,14 +2,15 @@ import pygame
 import random
 from circleshape import CircleShape
 from logger import log_event
-from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_MIN_RADIUS
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_MIN_RADIUS, ASTEROID_COLORS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+        self.color = ASTEROID_COLORS[random.randint(0, len(ASTEROID_COLORS)-1)]
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+        pygame.draw.circle(screen, self.color, self.position, self.radius, LINE_WIDTH)
 
     def update(self, dt):
         self.position += self.velocity * dt
@@ -26,5 +27,7 @@ class Asteroid(CircleShape):
 
         asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
         asteroid1.velocity = new_velocity1 * 1.2
+        asteroid1.color = self.color
         asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
         asteroid2.velocity = new_velocity2 * 1.2
+        asteroid2.color = self.color
